5d5679317c0758dbc76e6e818b6fd18e88fac9e4
minje0204 <minje@ajou.ac.kr>
2024-07-29 15:21:10 +0900

    init project

---
+++ src/main/java/com/_119/wepro/WeproApplication.java
@@ -0,0 +1,13 @@
+package com._119.wepro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeproApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/_119/wepro/WeproApplicationTests.java
@@ -0,0 +1,13 @@
+package com._119.wepro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeproApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
