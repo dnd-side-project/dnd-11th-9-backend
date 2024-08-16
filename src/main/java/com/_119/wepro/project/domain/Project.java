0385aebd741f1ca18dec74b98123347a70e9ec60
heejjinkim <06.hjhj.12@gmail.com>
2024-08-17 02:44:20 +0900

    feat: project id generatevalue 추가

--- src/main/java/com/_119/wepro/project/domain/Project.java
+++ src/main/java/com/_119/wepro/project/domain/Project.java
@@ -2,6 +2,8 @@ package com._119.wepro.project.domain;
 
 import com._119.wepro.global.BaseEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Getter;
 
@@ -10,5 +12,6 @@ import lombok.Getter;
 public class Project extends BaseEntity {
 
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 }
