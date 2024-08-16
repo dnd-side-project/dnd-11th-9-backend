dbd9c99bc0a36ed83f2ac854fa132f5360509563
heejjinkim <06.hjhj.12@gmail.com>
2024-08-17 02:55:42 +0900

    feat: ReviewForm 엔티티 추가
    Related to: #4

---
+++ src/main/java/com/_119/wepro/review/domain/ReviewForm.java
@@ -0,0 +1,51 @@
+package com._119.wepro.review.domain;
+
+import com._119.wepro.global.BaseEntity;
+import com._119.wepro.member.domain.Member;
+import com._119.wepro.project.domain.Project;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import java.util.List;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Getter
+@Builder
+public class ReviewForm extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id")
+  private Member member;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "project_id")
+  private Project project;
+
+  @OneToMany(mappedBy = "reviewForm", cascade = CascadeType.ALL)
+  private List<ReviewQuestion> reviewQuestions;
+
+  public static ReviewForm of(Member member, Project project) {
+    return ReviewForm.builder().member(member).project(project).build();
+  }
+
+  public void setReviewQuestions(List<ReviewQuestion> reviewQuestions) {
+    this.reviewQuestions = reviewQuestions;
+  }
+}
