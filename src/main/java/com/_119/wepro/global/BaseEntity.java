6a545f5a9c300f2e8dbb82c9097e9906bec21318
heejjinkim <06.hjhj.12@gmail.com>
2024-08-20 15:37:43 +0900

    refactor: 사용하지 않는 import문 제거

--- src/main/java/com/_119/wepro/global/BaseEntity.java
+++ src/main/java/com/_119/wepro/global/BaseEntity.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.auditing.config.AuditingConfiguration;
 
 import java.time.LocalDateTime;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
