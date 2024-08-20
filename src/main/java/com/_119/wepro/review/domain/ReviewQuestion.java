a61bd918f8562f2ca5c4ed230f9234422bc6c99e
heejjinkim <06.hjhj.12@gmail.com>
2024-08-20 15:39:24 +0900

    feat: ReviewQuestion 엔티티 추가
    related to: #4

---
+++ src/main/java/com/_119/wepro/review/domain/ReviewQuestion.java
@@ -0,0 +1,42 @@
+package com._119.wepro.review.domain;
+
+import com._119.wepro.global.BaseEntity;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Entity
+@Getter
+@Builder
+public class ReviewQuestion extends BaseEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "review_form_id")
+  private ReviewForm reviewForm;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "question_id")
+  private Question question;
+
+  public static ReviewQuestion of(ReviewForm reviewForm, Question question) {
+    return ReviewQuestion.builder()
+        .reviewForm(reviewForm)
+        .question(question)
+        .build();
+  }
+}
