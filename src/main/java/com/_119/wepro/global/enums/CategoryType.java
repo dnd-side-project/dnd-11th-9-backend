896a3817a7bf084b96ab7a451b939abe6e948bcc
heejjinkim <06.hjhj.12@gmail.com>
2024-08-20 15:01:40 +0900

    feat: 카테고리 추가

--- src/main/java/com/_119/wepro/global/enums/CategoryType.java
+++ src/main/java/com/_119/wepro/global/enums/CategoryType.java
@@ -11,7 +11,8 @@ public enum CategoryType {
   COLLABORATION("협업"),
   LEADERSHIP("리더십"),
   DOCUMENTATION("문서화"),
-  TIMEMANAGEMENT("시간관리"),
+  TIME_MANAGEMENT("시간관리"),
+  PROBLEM_SOLVING("문제해결"),
   DILIGENCE("성실성");
 
   private final String name;
