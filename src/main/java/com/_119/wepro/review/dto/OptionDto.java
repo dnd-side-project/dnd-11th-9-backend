49790988576c12349c4a95d766aaa39e81c4e571
heejjinkim <06.hjhj.12@gmail.com>
2024-10-05 23:07:28 +0900

    refactor: QuestionDto 위치 수정
    related to: #16

---
+++ src/main/java/com/_119/wepro/review/dto/OptionDto.java
@@ -0,0 +1,22 @@
+package com._119.wepro.review.dto;
+
+import com._119.wepro.review.domain.Option;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OptionDto {
+
+  private Long optionId;
+  private String content;
+
+  public static OptionDto ofWithoutId(Option option) {
+    return OptionDto.builder()
+        .content(option.getContent())
+        .build();
+  }
+}
+
--- src/main/java/com/_119/wepro/review/dto/response/QuestionResponse.java
+++ src/main/java/com/_119/wepro/review/dto/response/QuestionResponse.java
@@ -2,6 +2,7 @@ package com._119.wepro.review.dto.response;
 
 import com._119.wepro.global.enums.CategoryType;
 import com._119.wepro.review.domain.Question;
+import com._119.wepro.review.dto.QuestionDto;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -9,9 +10,11 @@ import lombok.Builder;
 import lombok.Getter;
 
 public class QuestionResponse {
+
   @Getter
   @Builder
-  public static class QuestionGetResponse{
+  public static class QuestionGetResponse {
+
     private List<QuestionInCategoryDto> allQuestions;
 
     public static QuestionGetResponse of(List<Question> questions) {
@@ -30,38 +33,19 @@ public class QuestionResponse {
 
   @Getter
   @Builder
-  private static class QuestionInCategoryDto{
+  private static class QuestionInCategoryDto {
 
     private CategoryType categoryType;
     private List<QuestionDto> questions;
 
     public static QuestionInCategoryDto of(CategoryType categoryType, List<Question> questions) {
       List<QuestionDto> questionDtos = questions.stream()
-          .map(QuestionDto::of)
+          .map(QuestionDto::ofWithoutOptionId)
           .toList();
 
       return QuestionInCategoryDto.builder()
           .categoryType(categoryType)
           .questions(questionDtos)
-          .build();
-    }
-  }
-
-  @Getter
-  @Builder
-  private static class QuestionDto {
-
-    private Long questionId;
-    private String question;
-    private List<String> options;
-
-    public static QuestionDto of(Question question) {
-      return QuestionDto.builder()
-          .questionId(question.getId())
-          .question(question.getContent())
-          .options(question.getOptions().stream()
-              .map(option -> option.getContent())
-              .toList())
           .build();
     }
   }
