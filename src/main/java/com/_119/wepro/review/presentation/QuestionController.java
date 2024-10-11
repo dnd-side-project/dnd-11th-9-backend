b0056182e17c4dd24e0cb95760f45af44e847fd1
heejjinkim <06.hjhj.12@gmail.com>
2024-10-11 21:16:18 +0900

    fix: requestBody -> requestParam 수정
    related to: #16

--- src/main/java/com/_119/wepro/review/presentation/QuestionController.java
+++ src/main/java/com/_119/wepro/review/presentation/QuestionController.java
@@ -1,16 +1,15 @@
 package com._119.wepro.review.presentation;
 
-import com._119.wepro.global.util.SecurityUtil;
-import com._119.wepro.review.dto.request.QuestionRequest.*;
-import com._119.wepro.review.dto.response.QuestionResponse.*;
+import com._119.wepro.global.enums.CategoryType;
+import com._119.wepro.review.dto.response.QuestionResponse.QuestionGetResponse;
 import com._119.wepro.review.service.QuestionService;
 import io.swagger.v3.oas.annotations.Operation;
-import jakarta.validation.Valid;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,12 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class QuestionController {
 
   private final QuestionService questionService;
-  private final SecurityUtil securityUtil;
 
   @Operation(summary = "카테고리에 해당하는 질문들 반환 API")
   @GetMapping("/categories")
-  public ResponseEntity<QuestionGetResponse> getQuestionsInCategories(@RequestBody @Valid QuestionGetRequest request){
-    securityUtil.getCurrentMemberId();
-    return ResponseEntity.ok(questionService.getQuestionsInCategories(request));
+  public ResponseEntity<QuestionGetResponse> getQuestionsInCategories(
+      @RequestParam List<CategoryType> categoryTypes) {
+    return ResponseEntity.ok(questionService.getQuestionsInCategories(categoryTypes));
   }
 }
